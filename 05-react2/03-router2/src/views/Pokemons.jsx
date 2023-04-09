74a9071290d05f43f9bb68af82cce48a0928be3a
arsato <arsatoji@gmail.com>
2023-04-08 23:55:27 -0400

    Agregado de card de preview en busqueda de pokemon

--- 05-react2/03-router2/src/views/Pokemons.jsx
+++ 05-react2/03-router2/src/views/Pokemons.jsx
@@ -6,8 +6,11 @@ const Pokemons = () => {
   const url = "https://pokeapi.co/api/v2/pokemon/?limit=811";
 
   const [data, setData] = useState([]);
+  const [data2, setData2] = useState([]);
   const [search, setSearch] = useState("");
   const [pokeName, setPokeName] = useState("");
+  const [pokeSprite, setPokeSprite] = useState("");
+  const [loaded, setLoaded] = useState(false);
 
   const navigate = useNavigate();
 
@@ -19,10 +22,28 @@ const Pokemons = () => {
   const getData = async () => {
     try {
       const res = await fetch(url);
-      let pokedata = await res.json();
-      setData(pokedata.results);
+      let pokelist = await res.json();
+      pokelist = pokelist.results;
+      pokelist = pokelist.map((val, i) => ({
+        id: i + 1,
+        name: val.name,
+        url: val.url,
+      }));
+
+      const promises = pokelist.map(async (poke) => {
+        const res = await fetch(poke.url);
+        const data = await res.json();
+        return data;
+      });
+
+      const pokedetails = await Promise.all(promises);
+      setData(pokelist);
+      setData2(pokedetails);
+      console.log(data2);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoaded(true);
     }
   };
 
@@ -30,67 +51,94 @@ const Pokemons = () => {
     getData();
   }, []);
 
-  const betterName = (data, index) => {
+  const betterName = (data) => {
     return (
-      (index + 1).toString().padStart(3, "0") +
+      data.id.toString().padStart(3, "0") +
       " - " +
       data.name.charAt(0).toUpperCase() +
       data.name.slice(1)
     );
   };
 
+  const getSprite = (poke) => {
+    const searchObject = data2.find((pokemon) => pokemon.name === poke);
+    let sprite = searchObject.sprites.front_default;
+    setPokeSprite(sprite);
+  };
+
+  const previewCard = () => {
+    return (
+      <div className="text-bg-dark preview">
+        <img src={pokeSprite} className="preview-img" />
+        <div className="card-text">
+          <p className="text-title">
+            {pokeName.charAt(0).toUpperCase() + pokeName.slice(1)}
+          </p>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="pokeView">
       <h1>Pokemon Data</h1>
-      <div className="pokeForm">
-        <div className="btn-group">
-          <button
-            type="button"
-            className="btn btn-secondary dropdown-toggle"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-            id="pokeButton"
-          >
+      {loaded ? (
+        <div className="pokeForm">
+          <div className="btn-group">
+            <button
+              type="button"
+              className="btn btn-secondary dropdown-toggle"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              id="pokeButton"
+            ></button>
+            <ul className="dropdown-menu">
+              <input
+                key="0"
+                className="buscador dropdown-item"
+                name="busqueda"
+                type="text"
+                placeholder={"Filtra por nombre del pokemon"}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              {data
+                .filter((value) => {
+                  if (value.name.toLowerCase().includes(search.toLowerCase())) {
+                    return true;
+                  }
+                  return false;
+                })
+                .map((pokemon, i) => (
+                  <li key={i + 1}>
+                    <button
+                      id={pokemon.id}
+                      value={pokemon.name}
+                      className="dropdown-item"
+                      href="#"
+                      onClick={({ target }) => (
+                        setPokeName(target.value),
+                        (document.getElementById("pokeButton").innerText =
+                          target.innerText),
+                        getSprite(pokemon.name)
+                      )}
+                    >
+                      {betterName(pokemon)}
+                    </button>
+                  </li>
+                ))}
+            </ul>
+          </div>
+          {pokeName ? <div className="poke-preview">{previewCard()}</div> : ""}
+          <button type="submit" className="btn btn-danger" onClick={irAPokemon}>
+            Ver Detalle
           </button>
-          <ul className="dropdown-menu">
-            <input
-              key="0"
-              className="buscador dropdown-item"
-              name="busqueda"
-              type="text"
-              placeholder={"Filtra por nombre del pokemon"}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            {data
-              .filter((value) => {
-                if (value.name.toLowerCase().includes(search.toLowerCase())) {
-                  return true;
-                }
-                return false;
-              })
-              .map((pokemon, i) => (
-                <li key={i + 1}>
-                  <button
-                    id={i + 1}
-                    value={pokemon.name}
-                    className="dropdown-item"
-                    href="#"
-                    onClick={({ target }) => (
-                      setPokeName(target.value),
-                      (document.getElementById("pokeButton").innerText =
-                        target.innerText)
-                    )}
-                  >
-                    {betterName(pokemon, i)}
-                  </button>
-                </li>
-              ))}
-          </ul>
         </div>
-        <button type="submit" className="btn btn-danger" onClick={irAPokemon}>
-          Ver Detalle
-        </button>
-      </div>
+      ) : (
+        <div className="alert alert-secondary">
+          <p>Cargando data!!!</p>
+          <p>Porfavor espere...</p>
+        </div>
+      )}
     </div>
   );
 };
